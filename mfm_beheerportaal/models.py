e1cd86801eb012f3ea6a7f90077519e8bfb8d4b2
Tim van Osch <timvosch@pollex.nl>
2019-03-22 10:55:48 +0100

    Format models

--- mfm_beheerportaal/models.py
+++ mfm_beheerportaal/models.py
@@ -8,14 +8,19 @@ import datetime
 
 class Organisation(models.Model):
     """An organisation"""
-    full_name = models.CharField(max_length=100)
-    short_name = models.CharField(max_length=100, blank=True)
+    full_name = models.CharField(
+        max_length=100)
+    short_name = models.CharField(
+        max_length=100,
+        blank=True)
 
 
 class MultiflexmeterVersion(models.Model):
     """Specifies a Multiflexmeter version"""
-    full_name = models.CharField(max_length=100)
-    short_name = models.CharField(max_length=100)
+    full_name = models.CharField(
+        max_length=100)
+    short_name = models.CharField(
+        max_length=100)
 
 
 class PhysicalLocationMixin(models.Model):
@@ -23,20 +28,46 @@ class PhysicalLocationMixin(models.Model):
     Defaults to (0,0,0)"""
     # Location
     latitude = models.DecimalField(
-        _('Latitude'), default=0, max_digits=11, decimal_places=8)
+        _('Latitude'),
+        default=0,
+        max_digits=11,
+        decimal_places=8)
     longitude = models.DecimalField(
-        _('Longitude'), default=0, max_digits=11, decimal_places=8)
+        _('Longitude'),
+        default=0,
+        max_digits=11,
+        decimal_places=8)
     altitude = models.DecimalField(
-        _('Altitude'), default=0, max_digits=11, decimal_places=8)
+        _('Altitude'),
+        default=0,
+        max_digits=11,
+        decimal_places=8)
 
 
 class Multiflexmeter(PhysicalLocationMixin, models.Model):
     """A Multiflexmeter device"""
-    identifier = models.CharField(_('Identifier'), max_length=100, unique=True)
-    description = models.TextField(_('Description'), max_length=100)
+    identifier = models.CharField(
+        _('Identifier'),
+        max_length=100,
+        unique=True)
+    description = models.TextField(
+        _('Description'),
+        max_length=100)
     owner = models.ForeignKey(
-        Organisation, on_delete=models.SET_NULL, null=True)
+        Organisation,
+        on_delete=models.SET_NULL,
+        null=True)
     version = models.ForeignKey(
+        MultiflexmeterVersion,
+        on_delete=models.SET_NULL,
+        null=True)
+    created = models.DateField(
+        _('Created on'),
+        default=datetime.date.today)
+    active = models.BooleanField(
+        _('Active'),
+        default=False)
+
         MultiflexmeterVersion, on_delete=models.SET_NULL, null=True)
     created = models.DateField(_('Created on'), default=datetime.date.today)
     active = models.BooleanField(_('Active'), default=False)
@@ -44,8 +75,16 @@ class Multiflexmeter(PhysicalLocationMixin, models.Model):
 
 class Gateway(PhysicalLocationMixin, models.Model):
     """A physical LoRa gateway"""
-    identifier = models.CharField(_('Identifier'), max_length=100, unique=True)
-    owner = models.ForeignKey(Organisation, on_delete=models.CASCADE)
-    description = models.TextField(_('Description'))
-    router = models.CharField(max_length=100)
-    brand = models.CharField(max_length=100)
+    identifier = models.CharField(
+        _('Identifier'),
+        max_length=100,
+        unique=True)
+    owner = models.ForeignKey(
+        Organisation,
+        on_delete=models.CASCADE)
+    description = models.TextField(
+        _('Description'))
+    router = models.CharField(
+        max_length=100)
+    brand = models.CharField(
+        max_length=100)
