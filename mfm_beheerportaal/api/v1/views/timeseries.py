a978c5384adfd1cffe21e88a2d91205c7c9a9088
Tim van Osch <timvosch@pollex.nl>
2019-03-25 18:03:40 +0100

    Add start, end url parameters

--- mfm_beheerportaal/api/v1/views/timeseries.py
+++ mfm_beheerportaal/api/v1/views/timeseries.py
@@ -17,6 +17,7 @@ class TimeserieViewSet(ViewSet):
         """
         Retrieve all measurements in a certain timerange for given devices
         """
+        start, end = self.get_timerange(request)
         return Response('Not implemented')
 
     @action(methods=['get'], detail=True, url_path='(?P<wns_id>[^/.]+)', url_name='WNS')
@@ -24,4 +25,22 @@ class TimeserieViewSet(ViewSet):
         """
         Retrieve a specific measurement in a certain timerange for given devices
         """
+        start, end = self.get_timerange(request)
         return Response('Not implemented')
+
+    def get_timerange(self, request):
+        """
+        Return the timerange denoted by `start` and `end` parameter
+        or throw if they don't exist.
+        """
+        start = request.GET.get('start', None)
+        end = request.GET.get('end', None)
+        if start is None:
+            raise KeyError('start time not present in request')
+        if end is None:
+            raise KeyError('end time not present in request')
+        return start, end
+
+class NoTimerangeError(Exception):
+    def __init__(self):
+        super().__init__('Invalid timerange supplied')
